0c00ab0410f935c9a08a557ea89f76e0835d445c
Daniel Lafontant <ldanieljajr@gmail.com>
2021-05-20 10:50:26 -0400

    delete button

--- controllers/articles.js
+++ controllers/articles.js
@@ -7,56 +7,15 @@ const db = require('../models')
 
 //////step 3
 router.get('/', async (req, res) => {
-// grab all the articles from the databse
-  const grabArticles = await db.article.findAll()
-// const grabArticles = await db.article.find().sort({createdAt: 'desc'})
-//   console.log(grabArticles);
-  ///now reder them to the second page named index.ejs
-//   and send  the result as grab article
-  res.render('articles/index', { articles: grabArticles });
-  
-})
-
-///create the show.ejs route for diaplay abd deleting
-router.get('/:idx',  async (req, res) => {
-console.log(req.params.idx)
-const grabArticle = await db.article.findOne({
-    where: {
-        id:req.params.idx
-    }
-})
-// ({ limit: 10, order: '"updatedAt" DESC' })
-res.render('articles/show', { article: grabArticle });
-// if (grabArticle ==null) res.redirect("/")
-  
-})
-
-
-// router.delete('/:idx', (req, res) =>{
-//     res.send("arttilces delete"  + req.params.idx)
-// })
-//to delete article selected by one
-router.delete('/:idx', async (req, res) =>{
-    res.send("arttilces delete"  + req.params.idx)
+    // grab all the articles from the databse
+    const grabArticles = await db.article.findAll()
+    // const grabArticles = await db.article.find().sort({createdAt: 'desc'})
+    //   console.log(grabArticles);
+    ///now reder them to the second page named index.ejs
+    //   and send  the result as grab article
+    res.render('articles/index', { articles: grabArticles });
 
 })
-
-
-
-// router.post('/', async (req, res) => {
-//     //test data from req.body
-//     const { title, date, description } = req.body
-//     console.log(title, date, description);
-//     // console.log(req.body)
-
-//     const newArticle = await db.article.update({ title, date, description })
-//     console.log(newArticle);
-//     // /to redirect the result to articles page
-//     res.redirect('/articles/show')
-// })
-
-
-
 
 ////step 1
 router.get('/new', (req, res) => {
@@ -65,13 +24,52 @@ router.get('/new', (req, res) => {
 })
 
 
+
+// ///step 4 create the show.ejs route for diaplay abd deleting
+router.get('/:idx', async (req, res) => {
+    console.log(req.params.idx)
+    const grabArticle = await db.article.findOne({
+        where: {
+            id: req.params.idx
+        }
+    })
+    // ({ limit: 10, order: '"updatedAt" DESC' })
+    res.render('articles/show', { article: grabArticle });
+    // if (grabArticle ==null) res.redirect("/")
+
+})
+
+
+// step 5 delete routr
+router.delete('/:idx', async (req, res)=>{ // const deleteArticle = await db.article.destroy({
+    console.log("You are here")
+    const deleteArticle = await db.article.destroy({
+        where: {
+            id: req.params.idx
+        }
+    })
+    console.log(deleteArticle)
+    res.redirect('/articles')
+})
+
+
+
+
+////step 6 for edit 
+
+
+
+
+
+
+
+
+
 ////step 2
 router.post('/', async (req, res) => {
     //test data from req.body
     const { title, date, description } = req.body
     console.log(title, date, description);
-    // console.log(req.body)
-
     const newArticle = await db.article.create({ title, date, description })
     console.log(newArticle);
     // /to redirect the result to articles page
